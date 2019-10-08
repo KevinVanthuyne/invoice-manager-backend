fcda6a6704ab005e0034bf2da6b86ec001437416
Kevin Vanthuyne <kevin.vanthuyne2@gmail.Com>
2019-10-08 18:48:34 +0200

    added get and getAll expenses endpoints

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ var router = require('express').Router();
 
 router.use(require('./customerRoutes'));
 router.use(require('./invoiceRoutes'));
+router.use(require('./expenseRoutes'));
 
 module.exports = router;
